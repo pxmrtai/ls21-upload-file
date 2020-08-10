1b9779089abb8092af32e16fca80e1ac2b563c01
Glitch (hello-express) <none>
2020-08-10 15:32:32 +0000

    👵🍄 Checkpoint ./server.js:20086806/1568 ./views/index.html:20086806/729 ./index.pug:20086806/1104 ./package.json:20086806/81 ./views/index.pug:20086806/1100 ./views/view.pug:20086806/110 ./db.json:20086806/342

--- server.js
+++ server.js
@@ -1,10 +1,61 @@
-// server.js
-// where your node app starts
+const express = require('express')
+const app = express()
+const port = 5000
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const shortid = require('shortid')
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ list: [] })
+  .write()
+
+app.get('/',(req,res) => {
+    res.render('index',{
+       list : db.get('list').value()
+    })
+})
+app.get('/:id',(req,res)=>{
+    var id = req.params.id;
+    var book = db.get('list').find({id:id}).value()
+    res.render('view',{
+        list: book
+    })
+})
+app.get("/:id/delete", function(req, res) {
+    db.get("list")
+     .remove({ id: req.params.id})
+     .write()
+     
+ res.redirect('/')
+})
+
+
+app.post('/',(req,res)=>{
+    req.body.id = shortid.generate();
+
+    db.get('list').push(req.body).write()
+    res.redirect('/')
+})
+app.post('/update',(req,res)=>{
+    db.get('list')
+    .find({ id:  req.body.id })
+    .assign({title: req.body.title})
+    .write()
+    res.redirect('/')
+})
+
+
+
 
 
 // listen for requests :)
