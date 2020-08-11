a404135772ad7dc5a465d09e089533c5ebe882a5
Glitch (hello-express) <none>
2020-08-11 09:41:21 +0000

    🐏👔 Checkpoint ./views/users/index.pug:20086806/563 ./routes/users.route.js:20086806/533

--- routes/users.route.js
+++ routes/users.route.js
@@ -2,7 +2,9 @@ var epxress = require('express');
 var router = epxress.Router();
 const db = require('../db')
 const shortid = require('shortid')
-
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 // Set some defaults (required if your JSON file is empty)
 db.defaults({ user: [] })
   .write()
@@ -12,6 +14,11 @@ router.get('/index',(req,res)=>{
     userList : db.get('user').value()
   })
 })
-
+router.post('/index',(req,res)=>{
+  res.render('user/index',{
+     
+   
+  })
+})
 
 module.exports = router;
