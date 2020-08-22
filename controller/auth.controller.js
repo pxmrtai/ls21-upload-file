eba39f26ef47f940bcdd67d825cf5e16d52895ad
Glitch (crud-users) <none>
2020-08-22 04:33:12 +0000

    🛤🕍 Checkpoint ./db.json:20086806/76 ./routes/auth.route.js:20086806/46 ./controller/auth.controller.js:20086806/682

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -3,4 +3,29 @@ const db = require('../db')
 
 module.exports.login = (req,res)=>{
   res.render('auth/login')
+
+}
+module.exports.postLogin = (req,res)=>{
+  var email = req.body.email
+ var user= db.get('user').find({email:email}).value()
+ var password = req.body.password
+ if(!user){
+     res.render('auth/login',{
+       errors:[
+         'User does not exist.'
+       ],
+       values: req.body
+     });
+   return;
+ }
+if(user.password !== password){
+  res.render('auth/login',{
+       errors:[
+         'Wrong password.'
+       ],
+    values: req.body
+     });
+   return;
+}
+  res.redirect('/')
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,4 +4,6 @@ const db = require('../db')
 var controller = require('../controller/auth.controller')
 router.get('/login',controller.login)
 
+router.post('/login',controller.postLogin)
+
 module.exports= router;
