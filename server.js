c92eea3e579eded372b7e8d6d475cb422ac7d960
Glitch (crud-users) <none>
2020-08-23 10:02:06 +0000

    🚴👫 Checkpoint ./server.js:20086806/732

--- server.js
+++ server.js
@@ -1,54 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
+var cookieParser = require('cookie-parser') 
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 const shortid = require('shortid');
 const adapter = new FileSync('db.json')
 const db = low(adapter)
+const bodyParser = require('body-parser')
+
+
 var userRoute = require('./routes/users.route');
 var authRoute = require('./routes/auth.route');
-
 var transaction = require('./routes/transaction.route')
 var counting = require('./middleware/count.middleware')
-var cookieParser = require('cookie-parser') 
 var controller = require ('./controller/bookList.controller')
-
 var authMiddleware = require('./middleware/auth.middleware')
-const bodyParser = require('body-parser')
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) 
+app.use(cookieParser())
 app.set('view engine', 'pug')
 app.set('views', './views')
-app.use(cookieParser())
-// app.use(counting);
 
-
-// Set some defaults (required if your JSON file is empty)
-// bạn tạo middleware xong rồi phải không
-// dạ
-// 
 db.defaults({ list: [] })
   .write()
  
 app.get('/',authMiddleware.requireAuth, controller.index)
-app.get('/book',authMiddleware.requireAuth,controller.listBook
-//         server
-)
-// app.get('/users/index',(req,res) => {
-//     res.render('users/index',{
-//        listUser : db.get('user').value()
-//     })
-// })
-
+app.get('/book',authMiddleware.requireAuth,controller.listBook)
 app.get('/:id',authMiddleware.requireAuth,controller.view)
 app.get("/:id/delete",authMiddleware.requireAuth, controller.deleteBook)
 
-
-
-app.post('/', controller.postIndex) // ?? cái này để làm gì vậy bạn ?
-
+app.post('/', controller.postIndex)
 app.post('/update',controller.update)
 
 
@@ -57,8 +41,6 @@ app.use('/transaction',authMiddleware.requireAuth, transaction)
 app.use('/auth',authRoute)
 app.use(express.static('public'))
 
-
-// listen for requests :)
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
