7fbee0918ecfef7ffa0cbb02e4cefa72d8abd13f
Glitch (crud-users) <none>
2020-08-23 05:11:42 +0000

    🏞🕰 Checkpoint ./middleware/email.middleware.js:20086806/52

--- middleware/email.middleware.js
+++ middleware/email.middleware.js
@@ -4,11 +4,11 @@ const db = require('../db')
 module.exports.notExist = (req,res,next)=>{
 
   var email = req.body.email
-  var user= db.get('user').find({email: req.body.email}).value()
+  var user= db.get('user').find({email: email}).value()
   console.log(user)
   console.log('asd'+ req.body.email)
 
-  if(user=undefined){
+  if(!user){
     res.render('transaction/create',{
       errors:[
         'email does not exist'
