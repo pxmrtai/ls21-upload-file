52ce00c25ce34aaeec80ba1aaa5615cc9e55e8cc
Glitch (crud-users) <none>
2020-09-01 08:12:59 +0000

    👼🔔 Checkpoint ./views/users/index.pug:20086806/2779:21008975/2604 ./controller/user.controller.js:20086806/24:21008975/3187

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,65 +1,66 @@
-const db = require('../db')
-const shortid = require('shortid')
-var emailMiddleware = require('../middleware/email.middleware')
-const bcrypt = require('bcrypt');
+const db = require("../db");
+const shortid = require("shortid");
+var emailMiddleware = require("../middleware/email.middleware");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
+const myPlaintextPassword = "s0//P4$$w0rD";
+const someOtherPlaintextPassword = "not_bacon";
 
-module.exports.customer = (req,res)=>{
-  var listBook = db.get('list').value()
-  var rentalList= db.get('rentalList').value()
-  res.render('userOnly/customer',{
-    list : db.get("list").value(),
-    rentalList : rentalList
-  })
- 
-}
-module.exports.userLogin = (req,res)=>{
-  
-  if(req.cookies.userId){
-  var user = db.get('user').find({id: req.cookies.userId}).value();
-  
-    
-  res.render('userOnly/index',{
-        user: user
-        
-    })
-    return
+module.exports.customer = (req, res) => {
+  var listBook = db.get("list").value();
+  var rentalList = db.get("rentalList").value();
+  res.render("userOnly/customer", {
+    list: db.get("list").value(),
+    rentalList: rentalList
+  });
+};
+module.exports.userLogin = (req, res) => {
+  if (req.cookies.userId) {
+    var user = db
+      .get("user")
+      .find({ id: req.cookies.userId })
+      .value();
+
+    res.render("userOnly/index", {
+      user: user
+    });
+    return;
   }
-  
-  
-}
-module.exports.index = (req,res)=>{
-   var page = parseInt(req.query.page) || 1;
-    var perPage = 2;
-    var start = (page-1)*perPage;
-    var end = (page - 1)* perPage + perPage
-  res.render('users/index',{
-   
-    currPage : parseInt(req.query.page),
+};
+module.exports.index = (req, res) => {
+  var page = parseInt(req.query.page) || 1;
+  var perPage = 2;
+  var start = (page - 1) * perPage;
+  var end = (page - 1) * perPage + perPage;
+  res.render("users/index", {
+    n: 1,
+    currPage: parseInt(req.query.page),
     // products: db.get('products').value().slice(start,end)
-    userList: db.get('user').drop(start).take(perPage).value()
-  })
-}
-module.exports.view = (req,res)=>{
-    var id = req.params.id;
-    var user = db.get('user').find({id:id}).value();
-  console.log(user)
-    res.render('users/view',{
-        user: user
-    })
-}
-module.exports.delete =  function(req, res) {
-     db.get("user")
-     .remove({ id: req.params.id})
-     .write()
-     
- res.redirect('/users/index')
-}
-module.exports.createUser =(req,res)=>{
-  res.render('users/create',{
-   
-  })
-  
-}
+    userList: db
+      .get("user")
+      .drop(start)
+      .take(perPage)
+      .value()
+  });
+};
+module.exports.view = (req, res) => {
+  var id = req.params.id;
+  var user = db
+    .get("user")
+    .find({ id: id })
+    .value();
+  console.log(user);
+  res.render("users/view", {
+    user: user
+  });
+};
+module.exports.delete = function(req, res) {
+  db.get("user")
+    .remove({ id: req.params.id })
+    .write();
+
+  res.redirect("/users/index");
+};
+module.exports.createUser = (req, res) => {
+  res.render("users/create", {});
+};
