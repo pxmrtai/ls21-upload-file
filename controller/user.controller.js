680bdb76b0267baae4538648b836fd206f2c1860
Glitch (crud-users) <none>
2020-09-01 08:58:10 +0000

    🌟⏳ Checkpoint ./views/users/index.pug:20086806/221 ./controller/user.controller.js:20086806/254

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -33,11 +33,16 @@ module.exports.index = (req, res) => {
   var start = (page - 1) * perPage;
   var end = (page - 1) * perPage + perPage;
   var maxPage =  Math.ceil(db.get("user").value().length / perPage);
+  var currPage= page
+  var nextPage= currPage+1
+  var prePage= currPage-1
+  var all=[prePage,currPage,nextPage]
   // mình lấy nhầm, lấy tổng số user có trong mảng / số user hiển hiện trên 1 trang.. ồ 
   res.render("users/index", {
     n: 1,
-    firstPage: page-2,
-    currPage: page,
+    currPage,
+    nextPage,
+    prePage,
     maxPage,
     // products: db.get('products').value().slice(start,end)
     userList: db
